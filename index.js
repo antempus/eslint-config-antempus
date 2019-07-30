5723ee45a8d1b9b24372023d6435c5292ca2683c
Robert Steele <rosteele@starbucks.com>
2019-07-29 20:30:34 -0700

    update liniting rules

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ module.exports = {
     "no-param-reassign": ["error", { props: false }],
     "no-use-before-define": ["error", { functions: false, classes: false }],
     "arrow-body-style": 0,
-    radix: ["error", "as-needed"],
     "jest/no-disabled-tests": "warn",
     "jest/no-focused-tests": "error",
     "jest/no-identical-title": "error",
@@ -23,7 +22,23 @@ module.exports = {
     "function-paren-newline": 0,
     "space-before-function-paren": 0,
     "func-names": 0,
-    "no-console": 0
+    "no-console": 0,
+    radix: ["error", "as-needed"],
+    quotes: 0,
+    "jsdoc/check-examples": 1,
+    "jsdoc/check-param-names": 1,
+    "jsdoc/check-tag-names": 1,
+    "jsdoc/check-types": 1,
+    "jsdoc/newline-after-description": 1,
+    "jsdoc/no-undefined-types": 1,
+    "jsdoc/require-hyphen-before-param-description": 1,
+    "jsdoc/require-param": 1,
+    "jsdoc/require-param-name": 1,
+    "jsdoc/require-param-type": 1,
+    "jsdoc/require-returns": 1,
+    "jsdoc/require-returns-check": 1,
+    "jsdoc/require-returns-type": 1,
+    "jsdoc/valid-types": 1
   },
   overrides: [
     {
@@ -33,5 +48,5 @@ module.exports = {
       }
     }
   ],
-  plugins: ["jest"]
+  plugins: ["jest", "jsdoc"]
 };
